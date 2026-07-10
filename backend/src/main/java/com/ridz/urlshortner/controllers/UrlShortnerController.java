0b0c3bb58daa1483f1955559d5cb74ec8e2e5881
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-07-10 17:56:30 +0200

    started with the DTO's

--- backend/src/main/java/com/ridz/urlshortner/controllers/UrlShortnerController.java
+++ backend/src/main/java/com/ridz/urlshortner/controllers/UrlShortnerController.java
@@ -1,11 +1,25 @@
 package com.ridz.urlshortner.controllers;
 
+import com.ridz.urlshortner.services.RateLimitService;
+import com.ridz.urlshortner.services.UrlShortnerService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 @Slf4j
+@RequiredArgsConstructor
 public class UrlShortnerController {
+    private final UrlShortnerService urlShortnerService;
+    private final RateLimitService rateLimitService;
+
+    @PostMapping("/shorten")
+    public ResponseEntity<?> shortenUrl(
+            
+    )
+
 }
--- backend/src/main/java/com/ridz/urlshortner/services/RateLimitService.java
+++ backend/src/main/java/com/ridz/urlshortner/services/RateLimitService.java
@@ -3,5 +3,5 @@ package com.ridz.urlshortner.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UrlShortnerService {
+public class RateLimitService {
 }

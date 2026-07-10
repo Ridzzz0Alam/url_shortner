f80bd95bb13b8cbe6413354bb85f9dadd3e85d0d
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-07-10 20:30:28 +0200

    building the Rest controller

--- backend/src/main/java/com/ridz/urlshortner/controllers/UrlShortnerController.java
+++ backend/src/main/java/com/ridz/urlshortner/controllers/UrlShortnerController.java
@@ -1,13 +1,20 @@
 package com.ridz.urlshortner.controllers;
 
+import com.ridz.urlshortner.dto.ShortenUrlRequest;
 import com.ridz.urlshortner.services.RateLimitService;
 import com.ridz.urlshortner.services.UrlShortnerService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -19,7 +26,23 @@ public class UrlShortnerController {
 
     @PostMapping("/shorten")
     public ResponseEntity<?> shortenUrl(
-            
-    )
+            @Valid @RequestBody ShortenUrlRequest request,
+            HttpServletRequest httpRequest
+    ) {
+        String clientIp = getClientIp(httpRequest);
+        if (!rateLimitService.isAllowed(clientIp)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body(Map.of());
+        }
+    }
+
+    private String getClientIp(HttpServletRequest httpRequest) {
+        String xForwardedFor = httpRequest.getHeader("X-Forwarded-For");
+        if (xForwardedFor != null & !xForwardedFor.isEmpty()) {
+            return xForwardedFor.split(",")[0].trim();
+        }
+
+        return httpRequest.getRemoteAddr();
+    }
 
 }
